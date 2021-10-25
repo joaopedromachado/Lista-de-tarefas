6ea314776dfa85e08ad5f3c52205fbce93c4d73d
João Pedro Machado <imjoaomachado@gmail.com>
2021-10-25 06:38:21 -0300

    delete comments

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
 let minhaUL = document.getElementById("minhaUL")
 
-//Sempre quando a página for carregada meu localStorage pega o valor da minhaUL e guarda dentro da lista
 onload = function () {
     let lista = localStorage.getItem("tarefas");
     minhaUL.innerHTML = lista
 }
 
-//Botão onclick, no qual eu adiciono as tarefas vindas do usuário.
 function addBtn() {
     let valorInput = document.getElementById("meuInput").value;
     let lista = document.createElement("li");
     let texto = document.createTextNode(valorInput);
 
-    lista.appendChild(texto); // Cria a lista com o valor inserido no input.
+    lista.appendChild(texto);
 
-// Condições:
     if (valorInput === '') {
-//Se enviado o valorInput vázio, recebo um alerta para inserir tarefa
+
         alert("Por favor, insira sua tarefa!!");
       }else if (valorInput.length > 30) {
-//Se enviado o valorInput maior a 20, recebo um alerta, cancelando a mesma.
+
           alert("Você excedeu o número máximo de caracteres permitidos")
       }else {
-//Caso o usuário obedeça as condições acima, a tarefa é indexada para a <li>
+
         document.getElementById("minhaUL").appendChild(lista);
       }
-//Após a execução da função o valor do input é retornado como vázio
+
         document.getElementById("meuInput").value = "";
 
-//Crio um botão indexado dentro da tag <li>
     let criarSpan = document.createElement("SPAN");
     let textoSpan = document.createTextNode("x");
     criarSpan.setAttribute("id", "spanBtn")
@@ -36,7 +32,6 @@ function addBtn() {
     lista.appendChild(criarSpan);
 
     criarSpan.setAttribute("onclick", "apagarTarefa(this)")
-
 
         guardarLista();
 }
@@ -48,13 +43,11 @@ function apagarTarefa(e) {
     guardarLista()
 }
 
-// Botão onclick, no qual eu apago todas as informações guardadas pelo localStorage
 function clearBtn() {
     localStorage.clear()
     minhaUL.innerHTML = ""
 }
 
-//Guardo as informações que o usuário inseriu no input. Não é necessário utilizar o JSON pois a UL não está em formato de objeto.
 function guardarLista() {
     let local = minhaUL.innerHTML
     localStorage.setItem("tarefas", local)
